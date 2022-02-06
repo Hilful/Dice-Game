d581842768b8f521d0c5867532c555356614d202
Hilful <hilful25@gmail.com>
2022-02-06 22:18:11 +0600

    second version of index.js that includes changes in dice

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 var randomNumber1 = Math.floor(Math.random()*6) + 1;
-console.log(randomNumber1);
+var randomDiceImage = "./images/dice" + randomNumber1 + ".png"
+var randomNumber2 = Math.floor(Math.random()*6) + 1;
+var randomDiceImage2 = "./images/dice" + randomNumber2 + ".png"
+document.querySelectorAll("img")[0].setAttribute("src",randomDiceImage);
+document.querySelectorAll("img")[1].setAttribute("src",randomDiceImage2);
